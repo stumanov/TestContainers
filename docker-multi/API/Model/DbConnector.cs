260bb639fe342344a671e69410fcd24308db452e
Сергей А. Туманов <s.tumanov@npo-pkrv.ru>
2018-11-02 13:39:39 +0300

    Initial commit

---
+++ docker-multi/API/Model/DbConnector.cs
@@ -0,0 +1,11 @@
+using System.Configuration;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Model
+{
+    public class DbConnector
+    {
+        
+    }
+}
